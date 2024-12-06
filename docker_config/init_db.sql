f08b0d3e29785535563d6eb178f7204369879e81
Artem Suvorov <artem.suvorov@gmail.com>
2024-12-06 13:11:48 -0500

    Initial Commit...

--- docker_config/init_db.sql
+++ docker_config/init_db.sql
@@ -1,2 +1,5 @@
 CREATE DATABASE IF NOT EXISTS `2remember`;
 CREATE USER     IF NOT EXISTS root IDENTIFIED BY 'root';
+CREATE USER     IF NOT EXISTS "ubuntu"@"localhost" IDENTIFIED BY "ubuntu";
+
+GRANT ALL PRIVILEGES ON * . * TO "ubuntu"@"localhost";
