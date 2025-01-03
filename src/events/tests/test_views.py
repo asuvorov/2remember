0bbb2d44a1acf198719226608d753a39012ec34d
Artem Suvorov <artem.suvorov@gmail.com>
2025-01-03 13:15:44 -0500

    Unittests Try-out...

--- src/events/tests/test_views.py
+++ src/events/tests/test_views.py
@@ -1,3 +1,41 @@
 """
 (C) 2013-2025 Copycat Software, LLC. All Rights Reserved.
 """
+
+import http.client
+import pytest
+
+from django.test import TestCase
+from django.test.client import RequestFactory
+
+from rest_framework.test import APIClient
+
+from events.models import (
+    Event,
+    Visibility)
+from tests.test_core import GenericUserTestCase
+
+
+# =============================================================================
+# ===
+# === TEST PRIVATE EVENTS.
+# ===
+# =============================================================================
+class PrivateEventTestCase(GenericUserTestCase):
+    """Test private Events."""
+
+    def setUp(self):
+        """Constructor."""
+        super().setUp()
+
+        self.event = Event.objects.create(
+            author=self.admin,
+            title="Private Event",
+            visibility=Visibility.PRIVATE)
+
+    def tearDown(self):
+        """Destructor."""
+        super().tearDown()
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
